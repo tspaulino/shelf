c460ea09e832273406be51e35e04a98738aeb3bc
Tiago Paulino <tspaulino@gmail.com>
2013-05-19 23:49:45 -0300

    First commit. Setup for initial gems (Rspec, HAML, bootstrap, devise)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shelf::Application.config.secret_token = '1dbf6a42356198b39a6657c787b1224fc3333046f096850bb8bb4bbc1c8ee88dd272f23583d2c991555bbd449dd94c6ca4eb684582f91d1b52dd2f91d55df118'
